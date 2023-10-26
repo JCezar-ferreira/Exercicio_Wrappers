c290584de4a1a37dc7d0238d5f08f90dcd221682
Cezar-Ferreira <113953683+Cezar-Ferreira@users.noreply.github.com>
2023-10-26 10:43:42 -0300

    Ecercicio

---
+++ src/Wrappers.java
@@ -0,0 +1,9 @@
+public class Wrappers {
+    public static void main(String args[]) {
+
+        int idade1 = Integer.valueOf(29);
+        long cpf = Long.valueOf(235684);
+        char letra = Character.valueOf('F');
+        }
+
+}
